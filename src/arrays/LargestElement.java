d537a16e63e98d38a3124518dc8c759ac2edc996
ShaliniP31 <178105699+ShaliniP31@users.noreply.github.com>
2026-01-02 22:00:02 +0530

    Arrays - Updated approaches for Largest element

--- src/arrays/LargestElement.java
+++ src/arrays/LargestElement.java
@@ -1,13 +1,24 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class LargestElement {
     public static void main(String[] args) {
-//        int[] arr = {2, 5, 1, 3, 0};
-        int[] arr = {8, 10, 5, 7, 9};
-        System.out.println("Largest element in array is " + maxElement(arr));
+        int[] arr = {2, 5, 1, 3, 0};
+//        int[] arr = {8, 10, 5, 7, 9};
+        System.out.println("Largest element in array is " + maxElement1(arr));
+        System.out.println("Largest element in array is " + maxElement2(arr));
+
     }
 
-    public static int maxElement(int[] arr) {
+    //Brute Force Approach - O(n log n)
+    public static int maxElement1(int[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length - 1];
+    }
+
+    //Optimal Approach O(n)
+    public static int maxElement2(int[] arr) {
         int max = Integer.MIN_VALUE;
         for (int a : arr) {
             if (a > max) {
